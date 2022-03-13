16ad8d4f2a69930f4a8fadb80bd6c7510991c4f7
Vlad Probst <vladlen.probst@gmail.com>
2022-03-13 10:42:00 +0200

    can sumNumbers

--- src/components/Vergtable.jsx
+++ src/components/Vergtable.jsx
@@ -1,13 +1,28 @@
 /** @format */
 
-import React from "react";
+import React, { useState } from "react";
 
 function Vergtable(props) {
+  function sumcheckedVitamims(vitamins) {
+    if (checked) {
+      props.setVitaminsSum(props.vitaminsSum - vitamins);
+    } else {
+      props.setVitaminsSum(props.vitaminsSum + vitamins);
+    }
+  }
+  let [checked, setChecked] = useState(false);
   return (
     <div>
       <h2>{props.name}</h2>
       <img width="100" height="100" src={props.image} alt={props.name}></img>
-      <p> vitamins C per 100gr: {props.vitamins}</p>
+      <p> vitamins C per 100gr: {props.vitamins} mg</p>
+      <input
+        type="checkbox"
+        onChange={(x) => {
+          sumcheckedVitamims(props.vitamins);
+          setChecked(!checked);
+        }}
+      ></input>
     </div>
   );
 }
